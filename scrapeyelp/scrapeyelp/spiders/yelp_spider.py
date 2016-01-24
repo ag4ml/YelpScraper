e61807a8f1fd438645be367aed38dabea08e2355
Abhishek Gupta <ag4cb@virginia.edu>
2016-01-24 15:36:06 -0500

    Finish writing yelp spider

--- scrapeyelp/scrapeyelp/spiders/yelp_spider.py
+++ scrapeyelp/scrapeyelp/spiders/yelp_spider.py
@@ -1,3 +1,5 @@
+# Abhishek Gupta (ag4cb@virginia.edu)
+
 import scrapy
 from scrapeyelp.items import Restaurant
 
@@ -8,18 +10,22 @@ class YelpSpider(scrapy.Spider):
     start_urls = [
         "http://www.yelp.com/search?cflt=restaurants&find_loc=Charlottesville%2C+VA%2C+USA"
     ]
-    count = 0
+    def __init__(self):
+        self.count= 0
 
     def parse(self, response):
-        count+=1
-        for blah in blah:
+        for listing in response.xpath('//li[@class="regular-search-result"]/div/div/div[@class="main-attributes"]'):
             item = Restaurant()
-            item["name"] = blah
-            item["rating"] = blah
-            item["price"] = blah
-            item["categories"] = blah
+            item["name"] = listing.xpath('.//a[@class="biz-name"]/span/text()').extract_first().encode('ascii', 'ignore')
+            item["rating"] = float(listing.xpath('.//div[@class="rating-large"]/i/@title').extract_first().split()[0])
+            choices = ["Inexpensive", "Moderate", "Pricey", "Ultra High-End"]
+            pricestr = listing.xpath('.//span[@class="business-attribute price-range"]/text()').extract_first()
+            item["price"] = choices[pricestr.count('$')-1] if type(pricestr)==unicode else "n/a"
+            item["categories"] = [str(i) for i in listing.xpath('.//span[@class="category-str-list"]/a/text()').extract()]
+            self.count+=1
             yield item
-        if count<10:
-            next_page = blah
+        if self.count < 100:
+            rel_url = response.xpath('//a[@class="page-option prev-next next"]/@href').extract_first()[:-2]
+            next_page = response.urljoin(rel_url+str(self.count))
             yield scrapy.Request(next_page, self.parse)
             
